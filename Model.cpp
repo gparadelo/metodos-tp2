94864d67a8adb3ec8ad43e3b75fa65a65fd6a48e
Mike Alche <mikealche@gmail.com>
2018-05-21 21:48:40 -0300

    fix

--- Model.cpp
+++ Model.cpp
@@ -218,7 +218,7 @@ double getSquaredNorm(const T &v1, const T &v2, int size) {
     for (int i = 0; i < size; ++i) {
         distance += pow((double) v1[i] - (double) v2[i], 2);
     }
-    return distance;
+    return sqrt(distance);
 }
 
 
